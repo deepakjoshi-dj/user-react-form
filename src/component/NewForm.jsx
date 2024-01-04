5b89973e4de5f149d6cb18ad6e3d18a0a35937e4
deepakjoshi-dj <dpkjoshi656.dj@gmail.com>
2024-01-04 06:58:28 +0530

    installed npm

--- src/component/NewForm.jsx
+++ src/component/NewForm.jsx
@@ -1,29 +1,42 @@
 import { Grid } from "@mui/material";
+import { useForm } from "react-hook-form";
 
 
 const NewForm = () => {
+    const {register, handleSubmit} = useForm();
 
+    const onSubmit = (data) => {
+        console.log(data)
+    }
 
     return (
         <>
             <Grid sx={{display:'flex', justifyContent:'space-around', alignItems:'center'}}>
                 <Grid width={'50%'}>
-                    <Grid>
-                        <label for="fullname">Full Name</label>
-                        <input type="text" id="fullname" />
-                    </Grid>
-                    <Grid>
-                        <label for="email">Email address</label>
-                        <input type="email" id="email" />
-                    </Grid>
-                    <Grid>
-                        <label for="phone">Phone Number</label>
-                        <input type="text" id="phone" />
-                    </Grid>
-                    <Grid>
-                        <label for="password">Password</label>
-                        <input type="password" id="password" />
-                    </Grid>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <Grid>
+                            <label for="fullname">Full Name</label>
+                            <input type="text" id="fullname" name="fullName" {...register("fullName")} />
+                        </Grid>
+                        <Grid>
+                            <label for="email">Email address</label>
+                            <input type="email" id="email" name="email" {...register("email")} />
+                        </Grid>
+                        <Grid>
+                            <label for="phone">Phone Number</label>
+                            <input type="text" id="phone" name="phone" {...register("phone")} />
+                        </Grid>
+                        <Grid>
+                            <label for="password">Password</label>
+                            <input type="password" id="password" name="password" {...register("password")} />
+                        </Grid>
+                        <Grid>
+                            <button type="submit">
+                                Submit
+                            </button>
+                        </Grid>
+
+                    </form>
 
                 </Grid>
             </Grid>
